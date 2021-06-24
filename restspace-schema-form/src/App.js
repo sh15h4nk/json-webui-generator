38bbdfce8e0dbb1116943221d31f05b582c15b74
sh15h4nk <shiva.shvvs@gmail.com>
2021-06-25 02:33:46 +0530

    Packages issue in linux

--- restspace-schema-form/src/App.js
+++ restspace-schema-form/src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import SchemaForm from "@restspace/schema-form";
+import SchemaForm from "schema-form";
 import "@restspace/schema-form/build/index.css";
 import { dataschema } from '../src/draft7_schema.json';
+import Container from '@material-ui/core/Container';
 
 function App() {
   var value = {};
   return (
     <div className="App">
+      <Container>
       <h3>RESTSPACE Schema Forms</h3>
       <SchemaForm
         schema={dataschema}
         value={value}
       />
+      </Container>
     </div>
   );
 }
