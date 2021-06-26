8b95918081803cbd37f3415c071f3fefc5c43918
sh15h4nk <shiva.shvvs@gmail.com>
2021-06-26 22:24:07 +0530

    load data on event | JSONFORMS-ECLIPSE

--- json-forms-eclipse/src/App.js
+++ json-forms-eclipse/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import { dataschema } from "../src/draft7.json";
-
 import {
   materialRenderers,
   materialCells,
@@ -9,7 +8,9 @@ import {
 import { Generate } from '@jsonforms/core';
 import React, { useState } from 'react';
 import { JsonForms } from '@jsonforms/react';
+import { Button } from 'react-bootstrap';
 
+var data = {}
 
 
 function App() {
@@ -25,6 +26,327 @@ function App() {
         cells={materialCells}
         onChange={({ errors, data }) => setData(data)}
       />
+      <Button onClick={() => setData({
+      "privacy": {
+        "3%\\Mi8n6?+3AF!BrXpcpZ(\"eQN5!]Gg^6O`?B*3qQR%Suo5": []
+      },
+      "services": [
+        {},
+        {
+          "hT9b_&J5nEzmrAR\\~o`c%K9aR@[+xjpFF x[vY0Y;%": 1497909958,
+          "externalUri": "8FQ=zsOZ1vm}r N,9TR 3*$0ep",
+          "internalURI": "+",
+          "B%*T'@I~P'Fccpe{O0,$B+j ()EY@)`d=OEcVxt%": false
+        },
+        {
+          "ws:1?Qn?": [
+            {
+              "U3\\#*": "&UJm\\/oyZ_*v{0iLm~oVuD4%?SZ$_",
+              ")ao%3Buv@HyB3NOvXNjG<4'+iNY$": {
+                "便Ǭྺ삓✈녺涨䣁쪅⚰ͷⱗ⧨킥鿵鼽": -870021414
+              }
+            },
+            267890464
+          ],
+          "큕紃嫶⫂墀圞ꛜ첆騠ي酐㼇倉ᱜ퉺敞냸讨◃鎇ួ訩퀡ᒸꌯ": -729302873
+        },
+        {
+          "externalUri": ">7IB!(-l(:|`q!:dn$",
+          "internalURI": "Qe`7q`}_6<5&1\\T0auYTfv{T3qf8a",
+          "serviceName": "`|5 HB@[.)",
+          "&twuLabn~gme'$+\\_H60$L87S}oBc0!\"": -1.7976931348623157e+308
+        },
+        {
+          "externalUri": "훸알嘆쉇ཹ뾵⮯ᧃ뛗ඁ䶜珩め쥶廊ᶐ挼橌ዾ屠妍谸靕᝚㿆ꈭ疞ꗑ㟙틻禈䅚醼謇雤",
+          "internalURI": "ᢿ衈䮂珴﵅",
+          "?轡巶쯬泑촃鴨뀬Ἒ萎헍ﮥ옶ಸ糞汌秌䴮槯럍߁៭磽萇魑Ѫ됖뙫筄": {
+            "`Gcp/ j`*R,G&:zlkOTuPXMk": false,
+            "qMY(fVb": [
+              {},
+              -1.7976931348623157e+308
+            ]
+          },
+          "serviceName": "쟭㩣䕉뼖ᔁ벣?禈屾畱⟧࿸ꜘ᭢⎆슾밹嚿쓠"
+        }
+      ],
+      "url": "http://fj95bZWO3IdJAÖUe8AjäaAöJÖlEFXKaCbU8yT-MVxDoüö6LHjsQÄäZÄ2_ä55BiiJIT_ämnH_duOba8AKZTEMüäNRN3UGÜvyDGc",
+      "states": {
+        "ﾠ쎂뚊盘缌臒枩΃압鄹큇済꺗궺寗偀਎톘햯콞": "kBplW)iv(4Aj>m0h\"0pCE[lmT(fQ:We9Rd:/G",
+        "description": "K|)^P",
+        "focus": [],
+        "lastchange": "\\\\RAdfGx'Nc0(XIf%7vtFQbd~u?BO$$dlGt*VXIPkig5g\"kggW@F",
+        "message": "xP\"7"
+      },
+      "techDetails": {
+        "legals": [
+          "splashpage",
+          "splashpage",
+          "institutions",
+          "institutions",
+          "vpnnational",
+          "institutions",
+          "zappscript",
+          "p2pblock"
+        ],
+        "!Bj3wrd]?C/~eGXItI;mw~9": -1.7976931348623157e+308,
+        "dns": [],
+        "updatemode": [
+          "none",
+          "none",
+          "autoupdate",
+          "autoupdate",
+          "autoupdate",
+          "autoupdate",
+          "manual",
+          "manual"
+        ],
+        "eCL3": [
+          {
+            " l<wzIS'LU{O4N||*we|fX'O'<px'8#nx": "Wn7>#&X>X",
+            ":`VU]Pr`.%b": "%[.@jHFyBg'sl=xR&(/ Kt5cj'",
+            "z:8~BgTw*_xbkhwY\\C[.mX24DLU|RPu=04Dm{x~,Jb2`h+q": -1430524039,
+            "7l$w(+h": -1.7976931348623157e+308
+          },
+          "8[4Y4HqL\"l@AemB*mzuKvg~eNe!?4=Sr<I'2o0"
+        ],
+        "networks": {
+          "ipv6": [
+            {},
+            {
+              "ܩ㉄ꆿ괕깠ꘊ艎??핍ᱤ뀳뤎訆葱뷨쏚杋鯪㢼盡喗㆖쪚ꝳৌ⯂申ЛṊ젓苬?": 2013985098
+            },
+            {
+              "TG,@GsX`t|<SIW": []
+            },
+            {
+              "t_5B)Z%^!!q^E|l": {},
+              "network": "    \u000b    \u000b \u000b    　  \t \u000b        \t  \n      \f                  \f   \u000b \r \u000b                      ﻿ 63:ED0::7df4:c7C:252.254.238.9\n \f \r       \n          　 \n \t   \t    \u000b\u000b\r ᠎᠎\f  \f \n /3"
+            },
+            {
+              "ꉉ᝙踃ᐖﵺ੥᪠䫬넀魎ᯛ᥂㶿䙎Ѣ㄰ﴦ赀": 108681048,
+              "network": "\r  \n 　    ﻿  \u000b﻿　     \f   \u000b   　\u000b   \n  ﻿\f  \r        \t       \n ﻿      ᠎ \n \t᠎ 72:f7AA:39::% Wtd@M'R&JUj);+R$%s7      \r \n﻿ 　 ᠎   \t         \t ᠎ ᠎ \r ﻿᠎/117"
+            }
+          ]
+        },
+        "firmware": {
+          "orgrestrictions": "Pf:,6Zh{p]j^!Z%='=r%Xh'pWce#s0>p|<@|C2C[.Jj7t^9p;<4]",
+          "url": "pnyY8't~@-v<UbZ&dTzn\"Oc3E<AFp{9ZL8vI1-z]dTV\"zGV%{63h"
+        }
+      },
+      "nodeMaps": [
+        {
+          "interval": "[fI<IQ8h0 'bEpfI"
+        },
+        {
+          "url": "wjJt=AFD3Qo}{ jX:}9zS8(?a.pSp]ey9vCNyP0H,XMYK&",
+          "technicalType": "ffsomething"
+        },
+        {
+          "mapType": "geographical",
+          "interval": "}tud[IYyQ@.WP~j|,Bw(Hvd#0r[(?m3MAa<03",
+          "Knm{_": 1602649553,
+          "url": "$#-D+IDErq1|,R=%Q_ivFn2duTZh-}cvY*CK_Cd^4>[qwzRZs|AihC|",
+          "'| \"+m1H=&Z,G-ZBjXQZZHRH&'wlvZncnc": {
+            "G*y#\"ThjNurz@wq$G#<fV zwi2!~UC0=Q@eK{:|@": {
+              "᷺Ἷ并䡅᝷誮襈絲": {
+                "5c~qwpIU2H;YWF^aUhyfdSsBUTED3a[$5~'Q$<": {
+                  "9VHP^VQd'T0u0CpM#j]}fSg942%pj06&w3boVKJ9\\18{CI": -1.7976931348623157e+308
+                },
+                "\\:`qmVVcJA<O": false
+              },
+              "빶㚢⛷ϵ൱酶赋??✨쫁⅜₍?ၗﭞ": -1788218152
+            }
+          },
+          "S@@K8": true
+        },
+        {
+          "interval": "1Im.I/aizjv\\mgnl,{$v",
+          "url": "6Yq&ak_>2d_b<`e=^Y89TOn7J0th,i*K8i+d5n"
+        },
+        {},
+        {
+          "&04_F{j[PZH1g%\"\\": 447679003,
+          "?|LZR:PN&i\"P73nNM6qV/D2)": -833515749,
+          "mapType": "structural",
+          "technicalType": "hopglass"
+        },
+        {
+          "mapType": ""
+        },
+        {
+          "Hxwe[EmG$=WKSO/pKj}2+<gC5~5cIfoN3Jm": "S6 P)So`&%_bz",
+          "mapType": "geographical",
+          "interval": ",,7\\{{-a$*g~b;>1}(Zd=U$rJ%",
+          "url": ")',537P!i8f%ttwdX1k8>6do-7h'8_\\bqz+&z\\",
+          "technicalType": ""
+        },
+        {
+          "mapType": "geographical",
+          "interval": "QYy-4j)^s{^@$`Qf;\"(8ocoB",
+          "technicalType": "ffsomething"
+        },
+        {
+          "臺첺蔬ᰗᣖꎹ⼰៝㐷筌ᙥ組悉瘇켁ᓬ뽫戴먉∽㧹氅ዪ࢝鵄⺣䃸槈䒢宮噻鏢惹??䷵䞑㹺ឥ阹": [
+            -1686337021,
+            {
+              "D~2|Ewi>n#}Q}ofI1(|O": false,
+              "me|~CB;.6/r)(": true
+            }
+          ],
+          "mapType": "structural",
+          "interval": "\\\"=;;w__Yw9<STGR)uZ]5K(xBBS|+4tgr7",
+          "technicalType": "ffsomething"
+        }
+      ],
+      "contact": {
+        "phone": "^v",
+        "facebook": "https://facebook.com/YObChKtBYöQbfipg_l",
+        "matrix": "https://matrix.to/#/!",
+        "webform": "j3`i!&M7lW4^4",
+        "identica": "http://g-bojqzwzhqqkrvmvnxppfssjndudgi-pgqzygpnmsrdwkbmbrzhwudziq.identi.ca/Pssxl0XY83PrFJuÖw48O3wCUSVX0Ü3An3t"
+      },
+      "name": "㧏Ꙕ컳捈걾㌏ꮤ툌韹䬥薎త䲺렄ഢᠽӠ䑧拨읶⧲⥤꾇ꇖ热坩",
+      "timeline": [
+        {
+          "林鬝ꅅ綣": -1.7976931348623157e+308,
+          "description": " YPmPDUsAzv`vS#O-n3hZ&TKo3|<}`>k4E8",
+          "uR_Wzyv>Z\"eN!BUL9t@dXC": -859503345,
+          "J?iAnTs6\\_j~}ubIvhF}F": -1.7976931348623157e+308
+        },
+        {
+          "](Jn[joBOE:{LTe&D[zJTo:[\\?y}2>'vLNJm{k": {
+            "U,+-l~F^\\>L:?cQ>lqIVf#+X/4Qw9J;%] 3)%o+Y#X\"3eP&>": -981005555,
+            "퍰뽱醶ᘶꚆ?쾢❥奶涯뷡?爱鸳셪撧帬蜞㏫ࡆꯀ솥읣댢◧氒⹿꠼⥞橦╈䐼뮷?ᯇꑊ䥴쾧免??": {},
+            "{;R`3`p@SG>.EO<7; :1|tP5@c+;7i(qcVyxf": [
+              "j)9\\s.e?\\:(qF",
+              "e\"xs[",
+              -1.7976931348623157e+308,
+              "#](;bNO|&roq''Qj`vy"
+            ],
+            "P6Jvrw": true
+          },
+          "description": "9\"!>+?i8<)10NthnsWH8L'+x,2WL{5ti6;mT2w d+tfRNWDHX<L",
+          "url": "♍ल꩞寢⎠떄ᎂ?朚Ꜳ눵矆緞媄㧾꠰⻫䨠홢푤㘄䄡",
+          "timestamp": "0038-8917122151198687835559504449052264534830231945684227636175161303245946112T6467808954974190128569080568849836051892453766131869406721259717821825153:19545"
+        },
+        {
+          "}": {
+            "yCPR": "g,muxSe:i@FgT@Xu9v-4jD}@c32#7 gE!,6IZMPo9Ph"
+          },
+          "url": "\\d<W.w,)DWS\\yiI]=",
+          "Ob0WU@": "PRqFv\">sx;A|7<9dm]_8By81H':7SGX?xWLt!E "
+        },
+        {
+          "DA0nOn5VCSfP=SB\\": "e\\66T4'8?%)",
+          "description": "rO>9@/ 2bMmvFTcD??4,R>i",
+          "url": "ꤦ炋㵆礧?糀堽⍖卾실ྑ絁ᚦퟴ疩螮鋜햚朩࿽ᰉ䘉䟒啞庺䰂㿣꥙烧밒அ쟿뒽蛆좦",
+          "timestamp": "4653-W65318337245766425592571633045566936219756Z51848466281579628463121526677751460289766461178385197972567029851702958327832784031240174"
+        },
+        {
+          "ߩ찥亞墒鯉턜侇惁腭몂졾Օ㕐슱踼⽘帠셺砃⩐庥冫橈Я⁌⺩㾧ꆥ氦圝㩹䟜禄ꁬ循꼬︽蟢ᗂ": [
+            true,
+            {},
+            -1924284679,
+            -1.7976931348623157e+308,
+            -1.7976931348623157e+308
+          ],
+          "fwHX": 2000538755,
+          "+IlOT)g3n3yS>c0TT\\x": [],
+          ":MsJ{|B(}Rxy9.r!_[ZLy6<_cHww%Ls9rfO?j": "`)GB'oEr-W8kZ)l}pXps&2\":ARs L368T_JZ#F!&wJL)}wFXopZ",
+          "_8/PUQ3bcWz<Ven(4:a!=U;9nokG`5>lmAN@242>pRS)-Y>YX": [
+            "` ",
+            -1073691388,
+            "$;&7@faYPSWE(01:tOvw5\"^ZY%5BuU)6\"_p>rs",
+            "Vl%lW HqyzQ!/}f@'s!G]6/D0[t.1;kBw?+?pHf|RY+Ap!+((ixqYvi<1",
+            false
+          ],
+          "銄د꫉齙裵ㇸ鞕?◉Ќ酜ꆌ％꘿兜पᮈ狲﬌㇇蜫幂벓黯?촙䘚踍᫒⚕뛚": [
+            "ze=5?]wG^'Ih'?L'MkmGm<J"
+          ],
+          "timestamp": "2803W9189569776859248079564814914787090759253031292199161049664Z-6301",
+          "PQa=*O'\\_wesmRV\"o&{z7(F`J.eq8CiaVS3": [
+            [
+              false,
+              438652260,
+              []
+            ],
+            1710502706,
+            -700357966,
+            [
+              false,
+              [
+                {},
+                false
+              ]
+            ],
+            "7*0!{2/9tvq7q\\Cp=@a=mYU62c]i."
+          ]
+        },
+        {
+          "url": "-v4j\\l|O'6Y4crB|$.:!!<C`4R0>]a85&O%@aslN-Xf9?H@lv<1,`I"
+        },
+        {
+          "description": "n)0Cw}.yQo_Ol*Z5jq$P?7C:8=Q7XimV|?,a@?",
+          "볰⤚䉘ᬌ밂뗏溾唹蕻굙譎곰俏櫦獘按봔᭍礧㫘궔眻ᰝጸ续뚅Ὅ䅲噏": true,
+          "url": "0&Ri<Zn2U6V;T q?gc9N]68>+>A~X)MKK{p<Og7X!sA(V"
+        },
+        {
+          "description": "?gju2XG3 {HTrX7ctA[GYJ-U5\"+Vh-%q[@T5Cy*DBX%_l]^jG_"
+        },
+        {
+          "description": ":#FETQ*2|#Y;4H!s}<cs!FD2=;q,I R>F,ITc,(z+@,ONC9x",
+          "?⏖﮴麆跌䆜崱⸂춺斬ᩚ꫃ᑩ鳊": "16AW=LyT~Q`*`b&=p\\`ukgHu1`B5X\"Y\\PGCt\"S7`J5o^\"5T<s2DH$~G$}f",
+          "url": "08Zc}<y",
+          "timestamp": "1149W98446794731637811986358Z5609918802337820941160560850451038464830644142434971446610233446"
+        }
+      ],
+      "feeds": [],
+      "location": {
+        "additionalLocations": [
+          {
+            "country": "PN",
+            "U1t,+a!ls=": true,
+            "address": {},
+            "city": ",shYmc'<bGd@xsdh(7zGC(?J;@Muk|]L$d['5GCIwD{JRE-O/",
+            "lon": "|4p`upqNu2}lx*w$LB"
+          }
+        ],
+        "lat": "P2xH"
+      },
+      "socialprojects": {
+        "number": -1554199652,
+        "x'?oVM+G9vi)G{gID8": {
+          "@3lf/Z(ycLuWy3/?M5xBR": "촨朂鉃퀆᭬",
+          "l XG*hNWc/\"e,zo<3xPb": ":Dc/UJXjRLlpkpdqD6ZQDI!_pQW f%gK3=X-1&VlaO[IA!9_ik"
+        },
+        "website": ":%~VHM>vlxi+OJ%S|a#<dUAocGX;*j|+*az",
+        "contact": "灶伧ࢰꝫ閽힛⠷嗜㧻놘蔜ᩮ싸?ꏿ㖁齫뛠ឈ嫔ऻꋧ᫊좛?飽욓䤝ᜐ巿ⅵ給騎뺜㤬滨?蛱᫝癣㎩ⶆ?习༉ꮸ⨂‶",
+        "y2Q[g$jj1|l.RDXlMn`u]=~|+tBryYi@[FH %,HY_DL5Vs": -1.7976931348623157e+308
+      },
+      "api": "0.4.3",
+      "support": {
+        "sFS\"7r@": "5X_,F^xRj$y]m)6q(>()W;w]9L&\\$(6yB\"X\"l8SjaI.)\"d:yn;AUVBO\\d",
+        "donations": {
+          "campaigns": [],
+          "bankaccount": {
+            "X%Q+gs8214w:xIH?>f>l'-<!vh 5b[4p\"$^@eC]%xhy-o^mQ": {
+              "E.`W wL7-f2_|R+95fqxNUeY&K}L": 107715240
+            },
+            "IBAN": "(]}J&@ql~!5M\"[&\"}(Y:Y_I'{fOgi6P/+*0soCd4&<e&m$A",
+            "pm<R`y%=4@PGx8Lo_;v19[!!RSE": [],
+            "usage": "D029F+0Xz99##\\3{5)$}]_gl~&y6V`0FojFQc#/$}Aot&ju]eU{&a~1"
+          },
+          "bK": "\"1f*%MN5c C.?+8mGb<YVkm;ce"
+        },
+        "+EeU'y$obS*#\\BknbD62L37SO?fZ_W0<EA_UY)).": true
+      }
+    }
+    )} color='primary'>
+        Initial Data
+      </Button>
+      <Button onClick={() => setData({})} color='primary'>
+        Clear form data
+      </Button>
       </center>
     </div>
   );
--- restspace-schema-form/src/App.js
+++ restspace-schema-form/src/App.js
@@ -1,21 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import SchemaForm from "schema-form";
+import SchemaForm from "@restspace/schema-form";
 import "@restspace/schema-form/build/index.css";
 import { dataschema } from '../src/draft7_schema.json';
-import Container from '@material-ui/core/Container';
 
 function App() {
   var value = {};
+  var collapsible = true;
   return (
     <div className="App">
-      <Container>
+      <center>
       <h3>RESTSPACE Schema Forms</h3>
       <SchemaForm
         schema={dataschema}
         value={value}
+        collapsible={collapsible}
       />
-      </Container>
+      </center>
     </div>
   );
 }
